afcc3ebdcb34220b5530d41c4fcbd59c480a8f45
Alexandre De Spiegeleer <alex-ds@live.be>
2023-02-20 15:29:00 +0100

    Remove opengl tutorial, moved to other repo

--- premake5.lua
+++ premake5.lua
@@ -41,38 +41,6 @@ include "deps/glfw.lua"
 include "deps/glad.lua"
 
 
--- Our first project, the static library
-project "Lib"
-    -- kind is used to indicate the type of this project.
-    kind "StaticLib"
-
-    -- We specify where the source files are.
-    -- It would be better to separate header files in a folder and sources
-    -- in another, but for our simple project we will put everything in the same place.
-    -- Note: ** means recurse in subdirectories, so it will get all the files in ExampleLib/
-    files "projects/lib/**"
-
-    -- We need GLFW, so we include it (include header file)
-    includedirs "deps/glfw/include"
-
-
--- The windowed app
-project "TestOpenGL"
-    kind "WindowedApp"
-    files "projects/test_opengl/**"
-
-    -- We also need the headers
-    includedirs "projects/lib"
-    links { "Lib", "GLFW" }
-
-    -- Now we need to add the OpenGL system libraries
-    filter { "system:windows" }
-        links { "OpenGL32" }
-
-    filter { "system:not windows" }
-        links { "GL" }
-
-
 project "OpenGL"
     kind "WindowedApp"
     language "C++"
