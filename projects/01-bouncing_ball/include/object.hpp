7864b8e42a30fff56ddc5709a072597470fc0865
Alexandre De Spiegeleer <alex-ds@live.be>
2023-05-21 22:05:44 +0200

    Move sphere and cube to classes

---
+++ projects/01-bouncing_ball/include/object.hpp
@@ -0,0 +1,42 @@
+#ifndef OBJECT_HPP
+#define OBJECT_HPP
+
+#include <vector>
+
+class Cube {
+    public:
+        Cube();
+        ~Cube();
+        void Draw() const;
+
+    private:
+        unsigned int VBO, EBO, VAO;
+        std::vector<float> vertices;
+        std::vector<unsigned int> indices;
+
+        void construct();
+        void setup();
+};
+
+class Sphere2 {
+    public:
+        Sphere2(unsigned int sectorCount=32, unsigned int stackCount=16);
+        ~Sphere2();
+        void Draw() const;
+
+    private:
+        std::vector<float> vertices;
+        std::vector<float> normals;
+        std::vector<float> texCoords;
+        std::vector<int> indices;
+        unsigned int VBO, VAO, EBO;
+
+        unsigned int sectorCount;
+        unsigned int stackCount;
+
+        void constructVertices();
+        void constructIndices();
+        void setup();
+};
+
+#endif
--- projects/01-bouncing_ball/src/main.cpp
+++ projects/01-bouncing_ball/src/main.cpp
@@ -14,6 +14,7 @@
 #include "shader.hpp"
 #include "camera.hpp"
 #include "utils.hpp"
+#include "object.hpp"
 
 
 const unsigned int SCR_WIDTH = 1920;
@@ -135,7 +136,7 @@ int main(int argc, char** argv)
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Bouncing Ball", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "PhysicsSim", NULL, NULL);
 
     if (window == NULL)
     {
@@ -160,105 +161,17 @@ int main(int argc, char** argv)
     }
 
     Shader blockShader("../projects/01-bouncing_ball/resources/shaders/vertex.vs", \
-            "../projects/01-bouncing_ball/resources/shaders/fragment.fs");
+                       "../projects/01-bouncing_ball/resources/shaders/fragment.fs");
 
     Shader lightShader("../projects/01-bouncing_ball/resources/shaders/light_cube.vs", \
-            "../projects/01-bouncing_ball/resources/shaders/light_cube.fs");
+                       "../projects/01-bouncing_ball/resources/shaders/light_cube.fs");
 
-    // ------------------------------------
-    //          SHAPE
-    // ------------------------------------
-    float vertices[] =
-    {
-        -0.5f, -0.5f, -0.5f,   0.0f,  0.0f, -1.0f,         // Front, bottom first up then     2---3
-         0.5f, -0.5f, -0.5f,   0.0f,  0.0f, -1.0f,         //                                 | \ |
-        -0.5f,  0.5f, -0.5f,   0.0f,  0.0f, -1.0f,         //                                 0---1
-         0.5f,  0.5f, -0.5f,   0.0f,  0.0f, -1.0f,
-
-        -0.5f, -0.5f,  0.5f,   0.0f,  0.0f,  1.0f,         // Back, bottom first up then
-         0.5f, -0.5f,  0.5f,   0.0f,  0.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,   0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,   0.0f,  0.0f,  1.0f,
-
-        -0.5f,  0.5f, -0.5f,   0.0f,  1.0f,  0.0f,          // TOP
-         0.5f,  0.5f, -0.5f,   0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,   0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,   0.0f,  1.0f,  0.0f,
-
-        -0.5f,  -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,           // Bottom
-         0.5f,  -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f,  -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f,  -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-
-         -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,          //  LEFT
-         -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-         -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-         -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-
-          0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,          //  Right
-          0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-          0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-          0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-    };
-
-    unsigned int indices[] = {
-        0, 1, 2, // first triangle
-        1, 2, 3, // second triangle
-
-        4, 5, 6,
-        5, 6, 7,
-
-        // 8, 9, 10,
-        // 9, 10, 11,
-
-        12, 13, 14,
-        13, 14, 15,
-
-        16, 17, 18,
-        17, 18, 19,
-
-        20, 21, 22,
-        21, 22, 23,
-    };
+    // Create a cube -> light and box to keep balls inside
+    Cube cube;
 
     std::vector<glm::vec3> cubePositions = {
         glm::vec3(0.0f,  0.0f,  0.0f),
     };
-
-
-    unsigned int VBO, EBO, VAO;
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    // light
-    unsigned int lightVAO;
-    glGenVertexArrays(1, &lightVAO);
-    glBindVertexArray(lightVAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // wireframe mode, good for debugging
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     // Define light stuff
     glm::vec3 lightPos(0.0f, 0.6f, 0.0f);
@@ -266,139 +179,25 @@ int main(int argc, char** argv)
 
     glm::vec3 blockColor(1.0f, 0.5f, 0.31f);
 
+    Sphere2 sphere;
+
+    unsigned int n_spheres = 50;
+    std::vector<Sphere> spheres(n_spheres);
+
+    for (unsigned int i=0; i!= n_spheres; ++i){
+        spheres[i].pos = glm::ballRand(0.25f);         // The box is centered at (0,0,0) and side of size of 0.5.
+        spheres[i].vel = glm::ballRand(0.25f);
+        spheres[i].color = glm::ballRand(0.5f)+0.5f;
+        spheres[i].radius = 0.05;
+        spheres[i].m = M_PI * spheres[i].radius * spheres[i].radius;
+    }
+
     // Transforms
     glm::mat4 proj;
     glm::mat4 view;
     glm::mat4 model;
 
     glEnable(GL_DEPTH_TEST);
-
-    // Create a sphere
-    std::vector<float> s_vertices;
-    std::vector<float> s_normals;
-    std::vector<float> s_texCoords;
-
-    unsigned int sectorCount = 32;
-    unsigned int stackCount = 16;
-    float radius = 1.0f;
-
-    float x, y, z, xy;                              // vertex position
-    float nx, ny, nz, lengthInv = 1.0f / radius;    // vertex normal
-    float s, t;                                     // vertex texCoord
-
-    float sectorStep = 2 * M_PI / sectorCount;
-    float stackStep = M_PI / stackCount;
-    float sectorAngle, stackAngle;
-
-    for(int i = 0; i <= stackCount; ++i)
-    {
-        stackAngle = M_PI / 2 - i * stackStep;        // starting from pi/2 to -pi/2
-        xy = radius * std::cos(stackAngle);             // r * cos(u)
-        z = radius * std::sin(stackAngle);              // r * sin(u)
-
-        // add (sectorCount+1) vertices per stack
-        // the first and last vertices have same position and normal, but different tex coords
-        for(int j = 0; j <= sectorCount; ++j)
-        {
-            sectorAngle = j * sectorStep;           // starting from 0 to 2pi
-
-            // vertex position (x, y, z)
-            x = xy * std::cos(sectorAngle);             // r * cos(u) * cos(v)
-            y = xy * std::sin(sectorAngle);             // r * cos(u) * sin(v)
-            s_vertices.push_back(x);
-            s_vertices.push_back(y);
-            s_vertices.push_back(z);
-
-            // normalized vertex normal (nx, ny, nz)
-            nx = x * lengthInv;
-            ny = y * lengthInv;
-            nz = z * lengthInv;
-            s_vertices.push_back(nx);
-            s_vertices.push_back(ny);
-            s_vertices.push_back(nz);
-
-            // vertex tex coord (s, t) range between [0, 1]
-            s = (float)j / sectorCount;
-            t = (float)i / stackCount;
-            s_texCoords.push_back(s);
-            s_texCoords.push_back(t);
-        }
-    }
-
-    // generate CCW index list of sphere triangles
-    // k1--k1+1
-    // |  / |
-    // | /  |
-    // k2--k2+1
-    std::vector<int> s_indices;
-    std::vector<int> s_lineIndices;
-    int k1, k2;
-    for(int i = 0; i < stackCount; ++i)
-    {
-        k1 = i * (sectorCount + 1);     // beginning of current stack
-        k2 = k1 + sectorCount + 1;      // beginning of next stack
-
-        for(int j = 0; j < sectorCount; ++j, ++k1, ++k2)
-        {
-            // 2 triangles per sector excluding first and last stacks
-            // k1 => k2 => k1+1
-            if(i != 0)
-            {
-                s_indices.push_back(k1);
-                s_indices.push_back(k2);
-                s_indices.push_back(k1 + 1);
-            }
-
-            // k1+1 => k2 => k2+1
-            if(i != (stackCount-1))
-            {
-                s_indices.push_back(k1 + 1);
-                s_indices.push_back(k2);
-                s_indices.push_back(k2 + 1);
-            }
-
-            // store indices for lines
-            // vertical lines for all stacks, k1 => k2
-            s_lineIndices.push_back(k1);
-            s_lineIndices.push_back(k2);
-            if(i != 0)  // horizontal lines except 1st stack, k1 => k+1
-            {
-                s_lineIndices.push_back(k1);
-                s_lineIndices.push_back(k1 + 1);
-            }
-        }
-    }
-
-    unsigned int VBO_s, VAO_s, EBO_s;
-
-    glGenVertexArrays(1, &VAO_s);
-    glGenBuffers(1, &VBO_s);
-    glGenBuffers(1, &EBO_s);
-
-    glBindVertexArray(VAO_s);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO_s);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * s_vertices.size(), &s_vertices[0], GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_s);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(float) * s_indices.size(), &s_indices[0], GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (0 * sizeof(float)));
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-
-    unsigned int n_spheres = 30;
-    std::vector<Sphere> spheres(n_spheres);
-
-    for (unsigned int i=0; i!= n_spheres; ++i){
-        spheres[i].pos = glm::ballRand(0.5f)-0.25f;
-        spheres[i].vel = glm::ballRand(2.0f)-1.0f;
-        spheres[i].color = glm::ballRand(1.0f);
-        spheres[i].radius = 0.05;
-        spheres[i].m = M_PI * spheres[i].radius * spheres[i].radius;
-    }
 
     unsigned int n_substeps = 5;
 
@@ -430,7 +229,7 @@ int main(int argc, char** argv)
         lightPos.x = amp * sin(time);
         lightPos.z = amp * cos(time);
 
-        // Select shader program, bind VAO and draw!
+        // Select shader program and set uniforms
         blockShader.use();
         blockShader.set3f("objectColor", blockColor);
         blockShader.set3f("lightColor", lightColor);
@@ -439,25 +238,19 @@ int main(int argc, char** argv)
         blockShader.setMat4f("proj", proj);
         blockShader.setMat4f("view", view);
 
-        glBindVertexArray(VAO);
         // Go through each box to render
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
         for (std::vector<glm::vec3>::size_type i=0; i!=cubePositions.size(); ++i)
         {
             model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
-            // model = glm::scale(model, glm::vec3(1.0f, 0.5f, 1.0f));
-            // float angle = 20.0f * (i+1);
-            // model = glm::rotate(model, glm::radians(angle*time), glm::normalize(glm::vec3(0.4f, 1.0f, 0.3f)));
             blockShader.setMat4f("model", model);
-            glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+            cube.Draw();
         }
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
         // Sphere
         // Use same shader as for block
-        glBindVertexArray(VAO_s);
-
         for (std::vector<Sphere>::size_type i = 0; i!=spheres.size(); ++i){
             // Draw
             model = glm::mat4(1.0f);
@@ -465,7 +258,7 @@ int main(int argc, char** argv)
             model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f) * spheres[i].radius);
             blockShader.setMat4f("model", model);
             blockShader.set3f("objectColor", spheres[i].color);
-            glDrawElements(GL_TRIANGLES, (unsigned int)s_indices.size(), GL_UNSIGNED_INT, 0);
+            sphere.Draw();
 
             // Create substeps for stability
             float dt = deltaTime / n_substeps;
@@ -474,14 +267,14 @@ int main(int argc, char** argv)
                 move(spheres[i], dt, cubePositions[0]);
 
                 // Check for collisions
-                for (std::vector<Sphere>::size_type j=0; j!=spheres.size(); ++j){
+                for (std::vector<Sphere>::size_type j=0; j != spheres.size(); ++j){
                     if (i != j)
                         collision(spheres[i], spheres[j]);
                 }
             }
         }
 
-        // Position the light!
+        // Draw the light!
         lightShader.use();
         model = glm::mat4(1.0f);
         model = glm::translate(model, lightPos);
@@ -490,8 +283,7 @@ int main(int argc, char** argv)
         lightShader.setMat4f("model", model);
         lightShader.setMat4f("proj", proj);
         lightShader.setMat4f("view", view);
-        glBindVertexArray(lightVAO);
-        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        cube.Draw();
 
         // swap buffers and poll IO events (key pressed/released, ...)
         // -----------------------------------------------------------
@@ -501,11 +293,6 @@ int main(int argc, char** argv)
 
     // glfw: terminate, clear all previous allocated GLFW resources
     // ------------------------------------------------------------
-    glDeleteVertexArrays(1, &lightVAO);
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
-
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
